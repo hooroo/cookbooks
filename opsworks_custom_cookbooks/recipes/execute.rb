b19b8e721e08cc406a93447a958e209719302d79
Ash McKenzie <ash@greenworm.com.au>
2014-01-22 15:19:15 +1100

    Fixes to ensure the ultimate run list order is good

--- opsworks_custom_cookbooks/recipes/execute.rb
+++ opsworks_custom_cookbooks/recipes/execute.rb
@@ -14,19 +14,33 @@ ruby_block("Compile Custom OpsWorks Run List") do
       recipe_run_list = node[:opsworks_custom_cookbooks][:recipes].dup.to_a
 
       override_run_list.each do |override_recipe, data|
+
         # Grab the direction (before/after) and recipe to look for
-        data.each do |direction, recipe_list|
+        data.each do |direction, recipe_list_to_search_for|
           next unless %w{ before after }.include?(direction)
 
-          if override_index = recipe_run_list.index { |x| x == override_recipe }
-            recipe_list.each do |recipe|
-              if new_index = recipe_run_list.index { |x| x == recipe }
-                # Remove override recipe from recipe_run_list
-                recipe_run_list.slice!(override_index)
+          if index_of_recipe_to_move = recipe_run_list.index { |x| x == override_recipe }
 
-                # Insert override recipe into correct position in recipe_run_list
-                new_index += 1 if direction == 'after'
-                recipe_run_list.insert(new_index, override_recipe)
+            recipe_list_to_search_for.each do |r|
+
+              index_of_recipe_in_run_list = recipe_run_list.index { |x| x == r }
+
+              # Only fiddle with the run list if the order is not already correct
+              #
+              if index_of_recipe_in_run_list
+                index_of_recipe_in_run_list += 1 if direction == 'after'
+
+                if index_of_recipe_in_run_list != index_of_recipe_to_move
+
+                  # Insert override recipe into correct position in recipe_run_list
+                  recipe_run_list.insert(index_of_recipe_in_run_list, override_recipe)
+
+                  # Remove override recipe from recipe_run_list
+                  index_of_recipe_to_move += 1 if index_of_recipe_to_move > index_of_recipe_in_run_list
+                  recipe_run_list.slice!(index_of_recipe_to_move)
+                end
+
+                break
               end
             end
           end
