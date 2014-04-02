ec3d7d161ff63abc51e7340c0d7d664398e69f3c
Ash McKenzie <ash@greenworm.com.au>
2014-04-02 14:24:49 +1100

    Properly escape PGSQL CREATE ROLE username

--- hooroo/recipes/postgres.rb
+++ hooroo/recipes/postgres.rb
@@ -61,7 +61,7 @@ node[:hooroo][:postgres][:users].each do |user|
     user "postgres"
     cwd "/tmp"
     code <<-EOH
-      psql postgres -c "CREATE ROLE \"#{db_username}\" WITH PASSWORD '#{db_password}' #{db_options}"
+      psql postgres -c "CREATE ROLE \\"#{db_username}\\" WITH PASSWORD '#{db_password}' #{db_options}"
     EOH
 
     only_if %Q{ test `psql postgres -t --no-align -c "SELECT 1 FROM pg_roles WHERE rolname='#{db_username}'"`x != "1x" }, :user => 'postgres'
@@ -82,7 +82,7 @@ node[:deploy].each do |application, x|
       user "postgres"
       cwd "/tmp"
       code <<-EOH
-        psql postgres -c "CREATE ROLE \"#{db_username}\" WITH PASSWORD '#{db_password}' LOGIN"
+        psql postgres -c "CREATE ROLE \\"#{db_username}\\" WITH PASSWORD '#{db_password}' LOGIN"
       EOH
 
       only_if %Q{ test `psql postgres -t --no-align -c "SELECT 1 FROM pg_roles WHERE rolname='#{db_username}'"`x != "1x" }, :user => 'postgres'
