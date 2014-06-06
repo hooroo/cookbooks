35d279c22ea7bb3a1aec4d998554b8bbfea169cf
scottharrisaus <scott@hooroo.com>
2014-06-06 11:35:32 +1000

    Create Deploy user before ssh_keys

--- hooroo/recipes/default.rb
+++ hooroo/recipes/default.rb
@@ -4,10 +4,10 @@
 # Copyright 2013, Hooroo
 
 include_recipe 'hooroo::custom_config'
+include_recipe 'hooroo::deploy_prepare'
 include_recipe 'hooroo::ssh_keys'
 include_recipe 'hooroo::locale'
 include_recipe 'hooroo::packages'
 include_recipe 'hooroo::users'
 include_recipe 'hooroo::wait_for_www_mount'
-include_recipe 'hooroo::deploy_prepare'
 include_recipe 'splunk'
