a5383b613768ad1d70c7e2b6da2d7343d41314c2
John Meredith <johnm@hooroo.com>
2014-09-08 14:44:59 +1000

    Can now add a public key to authorized_key for those keys marked as 'add_to_authorized_keys'

--- hooroo/recipes/ssh_keys.rb
+++ hooroo/recipes/ssh_keys.rb
@@ -7,18 +7,17 @@ require 'base64'
 
 if node[:ssh_keys]
   node[:ssh_keys].each do |key_detail|
+    authorized_keys = {}
 
     key_detail.each do |key_location, keys|
-
       next if keys.include?('layers') && (keys['layers'] & node[:opsworks][:instance][:layers]).empty?
 
-      public_key = Base64.decode64(keys['public'])
+      key_owner   = keys['owner']
+      public_key  = Base64.decode64(keys['public'])
       private_key = Base64.decode64(keys['private'])
-      auth_file = '/home/' + keys['owner'] + '/.ssh/authorized_keys'
 
-      # ensure the resting place exists
       directory File.dirname(key_location) do
-        owner     keys['owner']
+        owner     key_owner
         group     'root'
         mode      "0700"
         action    :create
@@ -26,29 +25,39 @@ if node[:ssh_keys]
       end
 
       file "#{key_location}.pub" do
-        owner     keys['owner']
+        owner     key_owner
         group     'root'
         mode      '0600'
         content   public_key
         action    :create
       end
 
-      file auth_file do
-        owner     keys['owner']
-        group     'root'
-        mode      '0600'
-        content   public_key
-        action    :create
+      # Keep track of those keys marked to be placed into authorized_keys
+      if keys.fetch('add_to_authorized_keys', false)
+        authorized_keys[key_owner] ||= []
+        authorized_keys[key_owner] << { public_key: public_key }
       end
 
       file key_location do
-        owner     keys['owner']
+        owner     key_owner
         group     'root'
         mode      '0600'
         content   private_key
         action    :create
       end
+    end
 
+    # Generate authorized_keys
+    authorized_keys.each_key do |owner|
+      template("/home/#{owner}/.ssh/authorized_keys") do
+        source  'authorized_keys.erb'
+        owner   owner
+        group   'root'
+        mode    '0600'
+        variables(
+          ssh_keys: authorized_keys[owner]
+        )
+      end
     end
   end
 end
