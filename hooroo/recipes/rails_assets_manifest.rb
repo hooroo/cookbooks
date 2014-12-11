bb0f835bf6af5d4495edf22669ac3c15464fe30b
John Meredith <johnm@hooroo.com>
2014-12-11 15:42:04 +1100

    Reconfigure hooroo::rails_assets_manifest recipe to use AWS credentials

--- hooroo/recipes/rails_assets_manifest.rb
+++ hooroo/recipes/rails_assets_manifest.rb
@@ -13,10 +13,10 @@ node[:deploy].each do |application, deploy|
   manifest_filename = "assets/manifest-#{deploy[:scm][:revision]}.json"
 
   aws_s3_file "#{deploy[:deploy_to]}/public/#{manifest_filename}" do
-    bucket "#{deploy[:s3_assets_bucket]}"
+    bucket node[:hooroo][:rails_assets_manifest][:bucket]
     remote_path manifest_filename
-    # aws_access_key_id     node[:custom_access_key]
-    # aws_secret_access_key node[:custom_secret_key]
+    aws_access_key_id     node[:hooroo][:rails_assets_manifest][:aws_access_key_id]
+    aws_secret_access_key node[:hooroo][:rails_assets_manifest][:aws_secret_access_key]
   end
 
 end
