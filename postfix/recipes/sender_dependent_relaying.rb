909c6a0b03b1a1af06704fe1bb9f48a9cc6423bb
John Meredith <johnm@hooroo.com>
2015-06-22 08:15:19 +1000

    Fix email_relays :bug:

--- postfix/recipes/sender_dependent_relaying.rb
+++ postfix/recipes/sender_dependent_relaying.rb
@@ -11,10 +11,6 @@ execute 'postmap-relayhosts_map' do
   action :nothing
 end
 
-email_relays = node['postfix']['email_relays'].inject({}) do |result, entry|
-  result[entry['email']] = entry.fetch('relay') { node['postfix']['main']['relayhost'] }
-  result
-end
 
 template '/etc/postfix/relayhost_map' do
   source 'relayhost_map.erb'
@@ -23,5 +19,10 @@ template '/etc/postfix/relayhost_map' do
   mode 0400
   notifies :run, 'execute[postmap-relayhosts_map]', :immediately
   notifies :restart, 'service[postfix]'
-  variables(:email_relays => email_relays)
+
+  email_relays = node['postfix']['email_relays'].inject({}) do |result, entry|
+    result[entry['email']] = entry.fetch('relay') { node['postfix']['main']['relayhost'] }
+    result
+  end
+  variables :email_relays => email_relays
 end
