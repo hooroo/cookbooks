166fe0e9d99d49ccde1fcf566456eb85bafa89ab
Scott Harris <scott@hooroo.com>
2016-02-08 15:58:49 +1100

    Adding cron recipe

---
+++ hooroo/recipes/cron.rb
@@ -0,0 +1,30 @@
+# Cookbook Name:: hooroo
+# Recipe:: cron
+#
+# Copyright 2013, Hooroocron 
+
+if node[:cron_entry]
+  node[:cron_entry].each do |cron_detail|
+    cron_detail.each do |name, items|
+      next if items.include?('command').nil?
+
+      command    = items['command']
+      minute     = items['minute']     || '*'
+      hour       = items['hour']       || '*'
+      dayofmonth = items['dayofmonth'] || '*'
+      month      = items['month']      || '*'
+      dayofweek  = items['dayofweek']  || '*'
+
+      cron 'Responsys SFTP' do
+        minute      "#{minute}"
+        hour        "#{hour}"
+        dayofmonth  "#{dayofmonth}"
+        month       "#{month}"
+        dayofweek   "#{dayofweek}"
+        command     "#{command}"
+        user        'deploy'
+      end
+      
+    end
+  end
+end
