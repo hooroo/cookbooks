9359dba54f822afddda57a0b651fd7d1c6420f34
Scott Harris <scott@hooroo.com>
2016-02-08 17:05:30 +1100

    Add postgresql repo to get 9.4

--- postgresql/recipes/ppa_pitti_postgresql.rb
+++ postgresql/recipes/ppa_pitti_postgresql.rb
@@ -8,3 +8,11 @@ apt_repository 'pitti-postgresql-ppa' do
   key '8683D8A2'
   action :add
 end
+
+apt_repository 'postgresql' do
+  uri 'http://apt.postgresql.org/pub/repos/apt/'
+  distribution 'precise-pgdg'
+  components %w(main)
+  key "https://www.postgresql.org/media/keys/ACCC4CF8.asc"
+  action :add
+end
