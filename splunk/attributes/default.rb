98d1d417c856266b5e2c6995a80c50d2053eecaa
Scott Harris <scott@hooroo.com>
2016-03-09 16:39:56 +1100

    Fixing url to splunk download

--- splunk/attributes/default.rb
+++ splunk/attributes/default.rb
@@ -1,5 +1,5 @@
 # coding: utf-8
 
-default['splunk']['version'] = '6.0.2'
-default['splunk']['remote_file'] = 'splunkforwarder-6.0.2-196940-Linux-x86_64.tgz'
-default['splunk']['remote_url'] = "http://www.splunk.com/page/download_track?file=#{default['splunk']['version']}/universalforwarder/linux/#{default['splunk']['remote_file']}&ac=&wget=true&name=wget&platform=Linux&architecture=x86&version=6.0&product=splunkd&typed=release&elq=463e95fc-b7b5-453d-bb81-e589d16ea93b"
+default['splunk']['version'] = '6.3.3'
+default['splunk']['remote_file'] = 'splunkforwarder-6.3.3-f44afce176d0-Linux-x86_64.tgz'
+default['splunk']['remote_url'] = "https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=#{default['splunk']['version']}&product=universalforwarder&filename=#{default['splunk']['remote_file']}&wget=true"
