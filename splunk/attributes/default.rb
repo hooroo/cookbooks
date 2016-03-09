39ef3237b4ff022ce8c47ca8a309c146e2cbe634
Scott Harris <scott@hooroo.com>
2016-03-09 17:54:19 +1100

    use http for download,  old opswork host has ssl issue connecting.

--- splunk/attributes/default.rb
+++ splunk/attributes/default.rb
@@ -2,4 +2,4 @@
 
 default['splunk']['version'] = '6.3.3'
 default['splunk']['remote_file'] = 'splunkforwarder-6.3.3-f44afce176d0-Linux-x86_64.tgz'
-default['splunk']['remote_url'] = "https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=#{default['splunk']['version']}&product=universalforwarder&filename=#{default['splunk']['remote_file']}&wget=true"
+default['splunk']['remote_url'] = "http://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=#{default['splunk']['version']}&product=universalforwarder&filename=#{default['splunk']['remote_file']}&wget=true"
